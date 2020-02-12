156b5e78c7a2eab849a9596a48d15b86926c888a
stephane fersing <stephane.fersing@gmail.com>
2020-02-12 11:51:18 +0100

    model valid

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5].freeze
   belongs_to :restaurant
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, presence: true, inclusion: { in: RATINGS }
 end
